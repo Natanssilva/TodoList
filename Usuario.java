9945b5112cd2917409bbfad75088af6a3dd9fa67
Natanssilva <natanssilva10@gmail.com>
2024-03-17 20:43:16 -0300

    first

--- Usuario.java
+++ Usuario.java
@@ -2,13 +2,17 @@ public class Usuario {
     private String email, senha, nome;
 
     //Constructor
-    public Usuario (String email,String senha,String nome){
+    public  Usuario (String email,String senha,String nome){
           this.email = email;
           this.senha = senha;
           this.nome = nome;
     }
 
+    // Getters e Setters (métodos de acesso)
+    // Métodos de validação, autenticação, etc.
 
 
-
+    public String getEmail(){
+        return email;
+    }
 }
