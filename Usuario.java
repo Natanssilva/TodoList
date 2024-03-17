567e72592671042d41d16ce741654a339d4ce762
Natanssilva <natanssilva10@gmail.com>
2024-03-17 00:15:31 -0300

    first commit

---
+++ Usuario.java
@@ -0,0 +1,14 @@
+public class Usuario {
+    private String email, senha, nome;
+
+    //Constructor
+    public Usuario (String email,String senha,String nome){
+          this.email = email;
+          this.senha = senha;
+          this.nome = nome;
+    }
+
+
+
+
+}
